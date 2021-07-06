2254358c79fffbd596fce7635e1c74f86ef5ad11
hamzayt577 <hamzayt577@gmail.com>
2021-07-06 15:02:02 +0400

    Coded half of the income and expense layouts

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const budgetInput = document.querySelector(".budget-input");
 const submitBudgetBtn = document.querySelector(".submit-budget");
 const totalBudgetParagraph = document.querySelector(".total-budget");
+// expense elements
+const inputExpenseAmount = document.querySelector(".expense-amount");
+const inputExpenseDescription = document.querySelector(".expense-description");
+const enterExpenseBtn = document.querySelector(".enter-expense");
+// income elements
+const inputIncomeAmount = document.querySelector(".income-amount");
+const inputIncomeDescription = document.querySelector(".income-description");
+const enterIncomeBtn = document.querySelector(".enter-income");
 
 let budget = parseFloat(budgetInput.value);
 
@@ -11,6 +19,28 @@ submitBudgetBtn.addEventListener("click", () => {
     budget = parseFloat(budgetInput.value);
     totalBudgetParagraph.innerHTML = `$${budget}`;
     budgetInput.value = "";
+  }
+});
+
+// Expense
+enterExpenseBtn.addEventListener("click", () => {
+  if (inputExpenseAmount.value === "" && inputExpenseDescription.value === "") {
+    alert("Please enter expense amount and description");
+  } else if (inputExpenseAmount.value === "0") {
+    alert("Expense amount cannot be 0");
+  } else {
+    // WILL IMPLEMENT THIS LATER
+  }
+});
+
+// Income
+enterIncomeBtn.addEventListener("click", () => {
+  if (inputIncomeAmount.value === "" && inputIncomeDescription.value === "") {
+    alert("Please enter income amount and description");
+  } else if (inputIncomeAmount.value === "0") {
+    alert("Expense amount cannot be 0");
+  } else {
+    // WILL IMPLEMENT THIS LATER
   }
 });
 
